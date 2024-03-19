f8ec2338d98dfc09defdd50595f7d4e759eb1f2b
shorenabakradze <136688121+shorenabakradze@users.noreply.github.com>
2024-03-19 19:30:05 +0400

    finished

---
+++ script.js
@@ -0,0 +1,19 @@
+const getAdvice = async () => {
+    try {
+        const response = await fetch('https://api.adviceslip.com/advice');
+        const { slip: { advice } } = await response.json();
+        return advice;
+    } catch (error) {
+        console.error('Error fetching advice:', error);
+        return 'Please try again later.';
+    }
+};
+
+const displayRandomAdvice = async () => {
+    const adviceElement = document.getElementById('advice');
+    adviceElement.textContent = 'Fetching advice...';
+    adviceElement.textContent = await getAdvice();
+};
+
+document.getElementById('button').addEventListener('click', displayRandomAdvice);
+displayRandomAdvice();
